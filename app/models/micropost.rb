cb31d12f052602d9c80dc7612ba8b85ebc8416cc
yoji0331 <smhsc251@yahoo.co.jp>
2015-06-09 19:46:05 +0900

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,13 @@
 class Micropost < ActiveRecord::Base
-	belongs_to :user
-	default_scope -> { order('created_at DESC') }
+  belongs_to :user
+  default_scope -> { order('created_at DESC') }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
 
-	validates :content, presence: true, length: { maximum: 140 }
-	validates :user_id, presence: true
+  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+    WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,10 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
+
     let(:user) { FactoryGirl.create(:user) }
-    
-    before(:each) do
+
+    before do
       sign_in user
       visit users_path
     end
@@ -16,10 +17,11 @@ describe "User pages" do
     it { should have_content('All users') }
 
     describe "pagination" do
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all) { User.delete_all }
 
-      it{ should have_selector('div.pagination') }
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
 
       it "should list each user" do
         User.paginate(page: 1).each do |user|
@@ -29,7 +31,9 @@ describe "User pages" do
     end
 
     describe "delete links" do
-      it{ should_not have_link('delete') }
+
+      it { should_not have_link('delete') }
+
       describe "as an admin user" do
         let(:admin) { FactoryGirl.create(:admin) }
         before do
@@ -50,8 +54,9 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo" ) }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar" ) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
@@ -61,8 +66,59 @@ describe "User pages" do
       it { should have_content(m1.content) }
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
-    end 
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+    end
   end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -100,10 +156,11 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success',text: 'Welcome') }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
+
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
@@ -114,16 +171,22 @@ describe "User pages" do
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
-      it { should have_link('change', href:'http://gravatar.com/emails') }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
     describe "with invalid information" do
-      let(:new_name) { "New Name" }
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
-        fill_in "Name", with: new_name
-        fill_in "Email", with: new_email
-        fill_in "Password", with: user.password
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
         fill_in "Confirm Password", with: user.password
         click_button "Save changes"
       end
@@ -131,8 +194,36 @@ describe "User pages" do
       it { should have_title(new_name) }
       it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
-      specify { expect(user.reload.name).to eq new_name }
+      specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
     end
   end
 end
